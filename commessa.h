1f130cc0f6393fdc28996e0a27d3924c378882b6
Sebastiano <sebacaste97@gmail.com>
2018-11-28 18:32:34 +0100

    inizio commessa

---
+++ commessa.h
@@ -0,0 +1,20 @@
+#define _COMMESSA_
+#ifndef _COMMESSA_
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Commessa{
+	private:
+		string committente;
+		int budget;
+		Data scadenza;
+		string luogo;
+		Report report;
+//		Fotografo fotografo
+	public:
+		
+};
+
+#endif
