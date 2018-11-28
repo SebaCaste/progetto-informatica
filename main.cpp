ab9b69f7c972d1eeaa3cd0cb35a5900090bf4c84
Sebastiano <sebacaste97@gmail.com>
2018-11-28 14:10:27 +0100

    risolto merge

--- main.cpp
+++ main.cpp
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
+
 #include <iostream>//ciao
 
-=======
-#include <iostream>// marco
->>>>>>> master
 //progetto informatica
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
