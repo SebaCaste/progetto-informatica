9db89c4a23831770b6c163cb624dfabdb11edddf
Alessandro <alessandro.goller@studenti.unitn.it>
2018-11-28 14:07:55 +0100

    //super

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
+#include <iostream>// marco
 //progetto informatica
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
 
 int main(int argc, char** argv) {
 	return 0;
